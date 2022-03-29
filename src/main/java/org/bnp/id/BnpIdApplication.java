54dfa931aa8b0493491e6054fc986667253a5c45
amadrazo <andrew.madrazo.contractor@hilton.com>
2022-03-28 18:06:53 -0700

    Removed unnecessary files and imports for security.

--- src/main/java/org/bnp/id/BnpIdApplication.java
+++ src/main/java/org/bnp/id/BnpIdApplication.java
@@ -3,7 +3,6 @@ package org.bnp.id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("org.bnp.id.repo")
@@ -11,8 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class BnpIdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BnpIdApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(BnpIdApplication.class, args);
+    }
 
 }
